765a709edbca8d222f72b23ea48a8474bc4fda87
Karl Loomes <kloomes@hotmail.com>
2020-05-08 12:59:17 +0100

    App render

---
+++ src/components/app.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="flat-list">
+          Flat List
+        </div>
+        <div className="map-container">
+          Map
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
